156ef7d6d0a13fa73d8def792c171820e1e2399c
sickdyd <roberto.reale.ja@gmail.com>
2024-07-20 10:44:29 +0900

    Use better key value for ExpandableItem

--- app/components/ExpandableItems.tsx
+++ app/components/ExpandableItems.tsx
@@ -3,6 +3,6 @@ import ExpandableItem from "./ExpandableItem";
 
 export default function ExpandableItems({ projects }: { projects: Project[] }) {
   return projects.map((project, id) => (
-    <ExpandableItem key={id} project={{ ...project }} />
+    <ExpandableItem key={project.title?.toString() || id} project={project} />
   ));
 }
