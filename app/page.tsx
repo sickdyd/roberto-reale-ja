f3dd27f403858291c98e1926106a8d0c927627b6
sickdyd <roberto.reale.ja@gmail.com>
2024-07-22 21:40:17 +0900

    Update homepage copy

--- app/page.tsx
+++ app/page.tsx
@@ -13,30 +13,15 @@ export default function Home() {
       <p className="mb-4">👋 Hello, welcome to my homepage!</p>
       <p className="mb-4">
         My name is Roberto Josef Antonio Reale. I’m a seasoned web developer
-        with <strong>over ten years of extensive experience</strong> in
+        with <strong>over fourteen years of extensive experience</strong> in
         creating, managing, and optimizing websites and web applications.
       </p>
       <p className="mb-4">
         Throughout my career, I have honed my skills in various programming
         languages and frameworks. I work mainly with <strong>JavaScript</strong>{" "}
-        (React, Next.js, Node.js) and <strong>Ruby on Rails</strong>. I am also
+        (React, Next.js, Node.js) and <strong>Ruby on Rails</strong>. I am
         proficient with <strong>HTML</strong> and <strong>CSS</strong> (vanilla
-        and Tailwind).
-      </p>
-      <p className="mb-4">
-        I'm contribuiting with a few{" "}
-        <LinkWithIcon
-          target="_blank"
-          href="https://www.npmjs.com/~sickdyd"
-          aria-label="npm packages on npmjs.com"
-        >
-          npm packages
-        </LinkWithIcon>{" "}
-        to the Open Source Community, feel free to check them out and give me
-        some feedback!
-      </p>
-      <p className="mb-4">
-        I am also a{" "}
+        and Tailwind). I am also a{" "}
         <LinkWithIcon
           target="_blank"
           href="https://www.credly.com/badges/08104359-709a-4150-b3b7-297d90f26ccf/public_url"
@@ -47,16 +32,28 @@ export default function Home() {
         .
       </p>
       <p className="mb-4">
-        Normally I work from Osaka, <strong>Japan</strong>. Every year I travel
-        back to my home country, <strong>Italy</strong>, and spend several weeks
-        or months working from there.
+        In my spare time I am contribuiting with a few{" "}
+        <LinkWithIcon
+          target="_blank"
+          href="https://www.npmjs.com/~sickdyd"
+          aria-label="npm packages on npmjs.com"
+        >
+          npm packages
+        </LinkWithIcon>{" "}
+        to the Open Source Community.
       </p>
       <p className="mb-4">
-        On a monthly basis I play live some of my original songs in some local
-        venues. Whenever there is the chance, I also go surfing! 🏄‍♂️
+        I am located in Osaka, <strong>Japan</strong>.
       </p>
       <p className="mb-4">
-        If you want to get in touch, hit me up with an email :-)
+        If you want to get in touch, hit me up with an{" "}
+        <LinkWithIcon
+          href="mailto:roberto.reale.ja@gmail.com"
+          aria-label="Send email to Roberto Reale"
+        >
+          email
+        </LinkWithIcon>{" "}
+        :-)
       </p>
     </div>
   );
