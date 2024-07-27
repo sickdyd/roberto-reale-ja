63f1bf89b6d0e7505b035e5eb8d4b1dcd7a63066
sickdyd <roberto.reale.ja@gmail.com>
2024-07-27 09:06:19 +0900

    Update carousel icons

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 
 export default function Carousel() {
   const logos = [
+    "html",
     "css",
     "tailwindcss",
-    "html",
     "javascript",
     "typescript",
+    "react",
+    "redux",
     "next.js",
     "node.js",
     "rubyonrails",
+    "aws",
     "kubernetes",
-    "AWS",
   ];
 
   return (
     <div className="flexflex-col mt-8 items-center justify-center">
       <p className="mb-4 text-center text-xs text-slate-600">
-        Bringing you the best web experience with
+        Bringing you the best web experience with:
       </p>
       <div className="flex justify-center gap-6 overflow-hidden">
         {logos.map((logo) => (
